2d5c2af1ea05bb6c9ec604c2cb5f9ae4302f82f5
yk <89748486+yashkhara0@users.noreply.github.com>
2026-03-30 19:51:59 +0100

    gradient_descent_3d_v2.py
    right now, it only tests a range of alphas. in v3 i want to implement auto alpha optimization, so finding best alpha on its own in terms of minimum loss but also comp efficiency

--- gradient_descent_3d.py
+++ gradient_descent_3d.py
@@ -1,20 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# initializing
+#initializing
 X = np.linspace(1,10,10)
 m = 3
 c = 7
-a = 0.2
 Y = m*X + c
-noisy = Y + np.random.normal(2,0.3,len(X))
+mu = 2
+noisy = Y + np.random.normal(mu,0.3,len(X))
 
-# to find losses at each point for later plot and later terminator
+
+#to find losses at each point for later plot and later terminator
 def lossFind(m,c,X,y):
     loss = np.mean((y - (m*X+c))**2)
     return loss
 
-# main func
+#main func
 def gradDesc(alpha,n,X,y):
     a = 0
     b = 0
@@ -24,41 +25,39 @@ def gradDesc(alpha,n,X,y):
     for i in range(n):
         aval.append(a)
         bval.append(b)
-        predictions = a*X+b
         losses.append(lossFind(a,b,X,y))
         grad_a = np.mean(-2*X*(y - (a*X + b)))
         grad_b = np.mean(-2*(y - (a*X + b)))
         a-=grad_a*alpha
         b-=grad_b*alpha
+        
+    return [(f"{a}x + {b}"), aval, bval, losses]
 
-    return(f"{a}x + {b}"), aval, bval, losses
+#creating set of output values for diff alphas
+def alphaMc(startAlpha, variance, step, X, Y, itern):
+    outputs = []
+    end = startAlpha+variance
+    while startAlpha<end:
+        vals = gradDesc(startAlpha, itern, X, Y)
+        outputs.append([startAlpha, vals[3]])
+        startAlpha+=(step)
 
-func, aval, bval, losses = gradDesc(0.001,10000,X,noisy)
+    return outputs
 
-print(func)
+# plotting the alphas against the losses to find best alpha 
+def outputInt(outputs):
+ 
+    best = min(outputs, key = lambda pair: min(pair[1]))
+    bestAlpha = best[0]
+    losses = best[1]
+    alphas = [l[0] for l in outputs]
+    minLosses = [min(lis[1]) for lis in outputs ]
+    plt.plot(alphas,minLosses)
+    plt.ylabel("losses")
+    plt.xlabel("alphas")
+    plt.title("minimum losses at each alpha value to show how alpha impacts net loss")
+    plt.show()
+    print(str(best))
 
-# --- Build loss surface ---
-A = np.linspace(min(aval), max(aval), 100)
-B = np.linspace(min(bval), max(bval), 100)
-
-A_grid, B_grid = np.meshgrid(A, B)
-Z = np.zeros_like(A_grid)
-
-for i in range(A_grid.shape[0]):
-    for j in range(A_grid.shape[1]):
-        Z[i, j] = lossFind(A_grid[i, j], B_grid[i, j], X, noisy)
-
-# --- Plot ---
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.plot_surface(A_grid, B_grid, Z)
-
-# plot gradient descent path
-ax.plot(aval, bval, losses, linewidth=2)
-
-ax.set_xlabel('a')
-ax.set_ylabel('b')
-ax.set_zlabel('loss')
-
-plt.show()
+#func call 
+outputInt(alphaMc(0.001, 0.01, 0.001, X, noisy, 1000))
